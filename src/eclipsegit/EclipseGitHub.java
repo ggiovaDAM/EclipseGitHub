5e3b646d9bbc226838f5da2a2aa2ef377ae43a5d
Guido <guido.giovadam@gmail.com>
2024-02-23 16:26:43 +0100

    Versión cero del proyecto

---
+++ src/eclipsegit/EclipseGitHub.java
@@ -0,0 +1,12 @@
+package eclipsegit;
+
+public final class EclipseGitHub {
+
+	public static void main(String[] args) {
+		for (int i=0; i<10; i++) {
+		    System.out.printf("\uD83D\uDE00");
+		}
+
+	}
+
+}
