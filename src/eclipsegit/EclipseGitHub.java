dd8aeb45eb84bf081e67b3599507027f0a3f4dbd
ggiovaDAM <94616132+ggiovaDAM@users.noreply.github.com>
2024-02-23 16:29:11 +0100

    Update EclipseGitHub.java

--- src/eclipsegit/EclipseGitHub.java
+++ src/eclipsegit/EclipseGitHub.java
@@ -4,8 +4,10 @@ public final class EclipseGitHub {
 
 	public static void main(String[] args) {
 		for (int i=0; i<10; i++) {
-		    System.out.printf("\uD83D\uDE00");
+			System.out.printf("\uD83D\uDE00");
+			System.out.println("\uD83D\uDE02");
 		}
+
 
 	}
 
